ed92995f517f3e46063e89c8b304092b6807e62f
Deeksha Dhatterwal <deekshadhatterwal150477@gmail.com>
2026-05-14 13:43:28 +0530

    final commit

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,103 +1,111 @@
-import streamlit as st 
+import streamlit as st
+
 
 
 def style_background_home():
-    st.markdown("""
-    <style>
-        .stApp {
-            background: #5865f2 !important;
-        }
 
-        div[data-testid="column"] {
-            background-color: #E0E3FF !important;
-            padding: 2.5rem 2rem !important;
-            border-radius: 2.5rem !important;
-            text-align: center;
-        }
-    
-        div[data-testid="stImage"] {
-            padding: 0 !important;
-            margin: 0 auto !important;
-        }
-
-        div[data-testid="stImage"] > div {
-            width: 100% !important;
-            display: flex !important;
-            justify-content: center !important;
-        }
-        div[data-testid="stImage"] img {
-            display: block !important;
-            margin-left: auto !important;
-            margin-right: auto !important;
-        }
-
-        div.stButton {
-            display: flex;
-            justify-content: center;
-            margin-top: 10px;
-        }
-
-    </style>
-    """, unsafe_allow_html=True)
-def style_background_dashboard():
     st.markdown("""
         <style>
-            .stApp {
-                background: #E0E3FF !important;
-            }
-        </style>
-    """, unsafe_allow_html=True)
 
+                .stApp {
+                    background: #5865F2 !important;
+                }
+
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                    }
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
+
+def style_background_dashboard():
+
+    st.markdown("""
+        <style>
+
+                .stApp {
+                    background: #E0E3FF !important;
+                }
+
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
+
+    
 
 def style_base_layout():
     st.markdown("""
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Outfit:wght@100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
+                
+         /* Hide Top Bar of streamlit */
+                
             #MainMenu, footer, header {
                 visibility: hidden;
             }
-
+                
             .block-container {
-                padding-top: 1.5rem !important;
+                padding-top:1.5rem !important;    
             }
 
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 3.5rem !important;
-                line-height: 0.9 !important;
-                margin-bottom: 0rem !important;
+                line-height:1.1 1important;
+                margin-bottom:0rem !important;
             }
-            h2{
+                
+
+            h2 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 2rem !important;
-                line-height: 0.9 !important;
-                margin-bottom: 0rem !important;
+                line-height:0.9 !important;
+                margin-bottom:0rem !important;
             }
-
+                
             h3, h4, p {
-                font-family: 'Outfit', sans-serif;
+                font-family: 'Outfit', sans-serif;    
             }
+                
 
-            button {
+            button{
                 border-radius: 1.5rem !important;
-                background: #5865F2 !important;
+                background-color: #5865F2 !important;
                 color: white !important;
                 padding: 10px 20px !important;
                 border: none !important;
                 transition: transform 0.25s ease-in-out !important;
-            }
+                }
 
-            button[kind="secondary"] {
-                background: #EB459E !important;
-            }
+            button[kind="secondary"]{
+                border-radius: 1.5rem !important;
+                background-color: #EB459E !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
-            button[kind="tertiary"] {
-                background: black !important;
-            }
+            button[kind="tertiary"]{
+                border-radius: 1.5rem !important;
+                background-color: black !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
-            button:hover {
-                transform: scale(1.05);
-            }
-        </style>
-    """, unsafe_allow_html=True)
+            button:hover{
+                transform :scale(1.05)}
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
