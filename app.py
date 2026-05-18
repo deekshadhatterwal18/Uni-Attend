e1be38a347c8b17951c30ed12c59f0cae6c6bfd1
Deeksha Dhatterwal <deekshadhatterwal150477@gmail.com>
2026-05-18 08:31:04 +0530

    fix: corrected streamlit app URL

--- app.py
+++ app.py
@@ -3,29 +3,21 @@ from src.screens.home_screen import home_screen
 from src.screens.teacher_screen import teacher_screen
 from src.screens.student_screen import student_screen
 
-def main():
-    
-    if 'login_type' not in st.session_state:
-        params = st.query_params
-        url_role = params.get("role", None)
-        if url_role in ['teacher', 'student']:
-            st.session_state['login_type'] = url_role
-        else:
-            st.session_state['login_type'] = None
 
-   
-    if st.session_state['login_type']:
-        st.query_params["role"] = st.session_state['login_type']
-    else:
-        st.query_params.clear()
+def main():
+    if 'login_type' not in st.session_state:
+        st.session_state['login_type'] = None
 
     match st.session_state['login_type']:
         case 'teacher':
             teacher_screen()
+
         case 'student':
             student_screen()
+
         case None:
             home_screen()
+
 
 if __name__ == "__main__":
     main()
